c35b069bfbbf0698ae6f8ff7f14ad515e3bf5186
Stefan Heimberg <kontakt@stefanheimberg.ch>
2018-11-24 19:42:32 +0100

    fix für regenerierung der ssh host keys

--- scripts/regenerate_ssh_host_keys.sh
+++ scripts/regenerate_ssh_host_keys.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "delete existing ssh host keys"
-/bin/rm -v /etc/ssh/ssh_host_*
+/bin/rm -vf /etc/ssh/ssh_host_*
 
 echo "reconfigure server to generate new host keys"
-dpkg-reconfigure openssh-server
+DEBIAN_FRONTEND=noninteractive dpkg-reconfigure openssh-server
 
 echo "restart ssh server"
 systemctl is-active --quiet ssh && systemctl restart ssh
